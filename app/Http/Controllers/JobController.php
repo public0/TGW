d4f101c8de5d17942778a8a1d941c7ab6431da59
Alex <alex.syscore@gmail.com>
2015-06-15 18:35:38 +0300

    job controller

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -34,7 +34,11 @@ class JobController extends Controller {
 	{
 		if(!in_array(21, $this->privsArray))
 			return redirect()->back();
-		$quizzes = Quiz::lists('name', 'id');
+		$quizzes = Quiz::where('category_id', '!=', 1)
+			->orWhere('category_id', 1)
+			->where('from', '<',\Carbon\Carbon::now())
+			->where('to', '>',\Carbon\Carbon::now())
+			->lists('name', 'id');
 		$officers = User::select('id', \DB::raw('CONCAT(name, " ", surname) AS name'))->whereIn('user_type_id', [3, 8])->lists('name','id') ;
 		return view('jobs.new_job', compact('quizzes', 'officers'));
 	}
@@ -82,7 +86,12 @@ class JobController extends Controller {
 		if(!in_array(23, $this->privsArray))
 			return redirect()->back();
 
-		$quizzes = Quiz::lists('name', 'id');
+		$quizzes = Quiz::where('category_id', '!=', 1)
+			->orWhere('category_id', 1)
+			->where('from', '<',\Carbon\Carbon::now())
+			->where('to', '>',\Carbon\Carbon::now())
+			->lists('name', 'id');
+
 		$officers = User::select('id', \DB::raw('CONCAT(name, " ", surname) AS name'))->whereIn('user_type_id', [3, 8])->lists('name','id') ;
 		$job = Job::find($id);
 
